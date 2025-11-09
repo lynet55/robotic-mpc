efa3137f954cb5daba387150a82f7c9fb529a17f
BB <>
2025-11-09 10:16:00 +0100

    added urdf loader

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-import yaml
-
-with open('NOC-project/params.yaml', 'r') as file:
-    params = yaml.safe_load(file)
-
-DH_params = params["UR5"]
-sim = params = params["sim"]
-
-
-
-# LOAD ROBOTICS MODEL
-# LOAD REFFERECNE TRAJECTORY
-
-
-
-def run_sim():
-
-    # state storage
-
-    #     // Cloosed loop simulation
-    #     for n in size(trajectory):
-    #         // STEP FORWARD TRAJECTORY
-    #         // CONTROLS: ACADOS MPC SOLVER
-    #         // MODEL: PLANT Robotics model
-    #         // INTEGRATOR: Homemade RK4 probly
-    #         // Set feedback
-
+from loader import UrdfLoader
 
 if __name__ == "__main__":
-    run_sim()
+    model = UrdfLoader("ur5")
+    
