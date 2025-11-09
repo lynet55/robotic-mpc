4fe8fab40da2cc966bf42c41b7c0c1a9ad1f5168
BB <>
2025-11-09 08:53:54 +0100

    model class not implemented yet

--- main.py
+++ main.py
@@ -17,31 +17,19 @@ def run_sim(controller, solver, surface, n_timesteps):
 
 if __name__ == "_main_":
 
+    #replace with surface class
     x = ca.SX.sym("x")
     y = ca.SX.sym("y")
     surface = 2*x**2 + 2*y**2 + 2*x*y + 2*x + 2*y + 2
-
-    #States
-    x = ca.SX.sym('x')
-    y = ca.SX.sym('y')
-    z = ca.SX.sym('z')
-    vx = ca.SX.sym('vx')
-    vy = ca.SX.sym('vy')
-    vz = ca.SX.sym('vz')
-    state = ca.vertcat(x, y, z, vx, vy, vz)
-
-    # Control inputs
-    ax = ca.SX.sym('ax')
-    ay = ca.SX.sym('ay')
-    az = ca.SX.sym('az')
-    control = ca.vertcat(ax, ay, az)
+    #####
 
     controller = mpc(
         surface = surface,
-        state = state,
-        control_input = control,
-        explicit_model= ur5.get_explicit_model(),
-        implisit_model= ur5.get_implcit_model()
+        state = model.state,
+        control_input = model.control,
+        explicit_model= model.get_explicit_model(),
+        implisit_model= model.get_implcit_model()
     )
 
+    #make yaml loader
     run_sim(controller, n_timesteps = 100)
--- model.py
+++
@@ -1,9 +0,0 @@
-# model_loader.py
-import casadi as ca
-
-class Model:
-    """
-    Defines and loads a simple 3D kinematic model.
-    """
-
-    def _init_(self):
