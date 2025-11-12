238a3b6741f4b102b64b5de8e25e088eeda89893
BB <>
2025-11-12 09:20:22 +0100

    minor changes to requriments, and main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from acados import Acados as mpc
+from loader import UrdfLoader as robotModel
 import casadi as ca
 
 
@@ -7,29 +8,31 @@ def run_sim(controller, solver, surface, n_timesteps):
 
     for i in range(n_timesteps):
 
-        solver.set(0, 'lbx', x_current)
-        solver.set(0, 'ubx', x_current)
+        # solver.set(0, 'lbx', x_current)
+        # solver.set(0, 'ubx', x_current)
         
-        solver.solve()
-        u_opt = solver.get(0, "u")
+        # solver.solve()
+        # u_opt = solver.get(0, "u")
 
   
 
 if __name__ == "_main_":
+    
+    robot_model = robotModel("ur5")
 
     #replace with surface class
     x = ca.SX.sym("x")
     y = ca.SX.sym("y")
-    surface = 2*x**2 + 2*y**2 + 2*x*y + 2*x + 2*y + 2
+    
     #####
 
-    controller = mpc(
-        surface = surface,
-        state = model.state,
-        control_input = model.control,
-        explicit_model= model.get_explicit_model(),
-        implisit_model= model.get_implcit_model()
-    )
+    # controller = mpc(
+    #     surface = surface,
+    #     state = model.state,
+    #     control_input = model.control,
+    #     explicit_model= model.get_explicit_model(),
+    #     implisit_model= model.get_implcit_model()
+    # )
 
     #make yaml loader
-    run_sim(controller, n_timesteps = 100)
+    run_sim(n_timesteps = 100)
