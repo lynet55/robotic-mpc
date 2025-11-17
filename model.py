2aa0cfc004e0288bc995dd5549e009032a5186b7
Cristiano <cristiano.sartini1@gmail.com>
2025-11-17 11:20:26 +0100

    Model: Finalization of the class & validation
    Added exact simulation method (lagrange formula) and rk4 integration method, enhance efficiency of the class by isolating the output equation computation from the state integration, and finally created the module model_validation for comparison of integration methods in terms of accuracy VS function evaluation.
    
    Signed-off-by: Cristiano <cristiano.sartini1@gmail.com>

--- model.py
+++ model.py
@@ -1,49 +1,25 @@
 import numpy as np
 import pinocchio as pin
-from pathlib import Path
+import time
 from loader import UrdfLoader
-
+from visualization import Visualization
+from scipy.linalg import expm
 
 # This module implements the state space robot model, taking as reference the
 # ur5 6 d.o.f industrial manipulator, using different simulation approaches.
-
 class Robot:
-    '''
-    ROOT = Path(__file__).parent
-    URDF_DIR = ROOT / "ur_description/urdf"
-    '''
     
-    def __init__(self, Ts:float, T, Tu, z0, u_step, urdf_loader: UrdfLoader) -> None:
+    def __init__(self, Ts:float, T:float, Tu:float, z0, u_step:float, urdf_loader: UrdfLoader) -> None:
         self._Ts = Ts
         self._T = T
         self._Tu = Tu
         self._z0 = z0
         self._u_step = u_step
         self._urdf_loader = urdf_loader
-        '''
-        self._urdf_file_path = Robot.URDF_DIR / urdf_file_name
-        self._mesh_dir = [str(self._urdf_file_path.parent)]
 
-        try:
-            self._model, vmodel, cmodel = pin.buildModelsFromUrdf(self._urdf_file_path, package_dirs=self._mesh_dir)
-            print(f"URDF successfully loaded: {self._urdf_file_path}")
-            print(f"nq = {self._model.nq}, ngeoms(vis) = {vmodel.ngeoms}, ngeoms(col) = {cmodel.ngeoms}")
-            self._data = self._model.createData()
-
-        except FileNotFoundError as e:
-            print(f"File not found: {e.filename}")
-
-        except ValueError as e:
-            # Invalid URDF or missing mesh files
-            print(f"Error while parsing the URDF or building the model:\n{e}")
-
-        except Exception as e:
-            # Generic catch for unexpected errors (e.g., missing libraries, permission issues)
-            print(f"Unexpected error while loading the model:\n{type(e).__name__}: {e}")
-
-        else:
-            print("Pinocchio model and data successfully created.")
-    '''
+        self._N = int(self._T/self._Ts)
+        self._Nu = int(self._Tu/self._Ts)
+   
     @property
     def Ts(self):
         return self._Ts
@@ -55,6 +31,7 @@ class Robot:
         if not isinstance(new_Ts, float):
             raise TypeError(f"Expected type float, got {type(new_Ts).__name__}.")
         self._Ts = new_Ts
+        self._update_N_and_Nu()
 
     @property
     def T(self):
@@ -67,6 +44,7 @@ class Robot:
         if not isinstance(new_T, float):
             raise TypeError(f"Expected type float, got {type(new_T).__name__}.")
         self._T = new_T
+        self._update_N_and_Nu()
 
     @property
     def Tu(self):
@@ -79,6 +57,7 @@ class Robot:
         if not isinstance(new_Tu, float):
             raise TypeError(f"Expected type float, got {type(new_Tu).__name__}.")
         self._Tu = new_Tu
+        self._update_N_and_Nu()
 
 
     @property
@@ -101,6 +80,10 @@ class Robot:
             raise TypeError(f"Expected type float, got {type(new_u).__name__}.")
         self._u_step = new_u
 
+    def _update_N_and_Nu(self):
+        self._N = int(self._T / self._Ts)
+        self._Nu = int(self._Tu / self._Ts)
+
     def _forward_kinematics(self, q):
         model = self._urdf_loader.model
         data = self._urdf_loader.data
@@ -114,90 +97,161 @@ class Robot:
 
         quat = pin.Quaternion(R).coeffs()
         
-        return np.hstack((p, quat))                 # [x_e, y_e, z_e, x, y, z, w]
-
-    
+        return np.hstack((p, quat))                # [x_e, y_e, z_e, x, y, z, w]
 
     def _diff_kinematic(self, q, qdot):
         model = self._urdf_loader.model
         data = self._urdf_loader.data
         fee = self._urdf_loader.fee
         
-
         # The WORLD frame is retrieved by Pinocchio from the urdf, where by default is set as coincident with the robot base frame
         J = pin.computeFrameJacobian(model, data, q, fee, pin.ReferenceFrame.WORLD)
 
-        return J@qdot                                # [vx, vy, vz, wx, wy, wz]
+        return J@qdot                              # [vx, vy, vz, wx, wy, wz]
 
-    def _continuos_time_model(self, z, u, Wcp):
+    def _continuos_time_state(self, z, u, Wcv):
         z_dot = np.zeros((12,), dtype=np.float64)
-        y = np.zeros((13,), dtype=np.float64)
+        z_dot[:6] = z[6:]                          # Joint positions state equations
+        z_dot[6:] = - Wcv @ z[6:] + Wcv @ u        # Joint speeds state equations
+        return z_dot
 
-        z_dot[:6] = z[6:]                           # Joint positions state equations
-        z_dot[6:] = - Wcp@z[6:] + Wcp@u             # Joint speeds state equations 
-        y[:7] = self._forward_kinematics(z[:6])     # End effector pose output equations
-        y[7:] = self._diff_kinematic(z[:6],z[6:])   # End effector velocities output equations
+    def euler_simulation(self, wcv):
+        start = time.perf_counter()
+        Wcv = np.diag(wcv)
 
-        return z_dot, y
-
-    def ffd_simulation(self, wcp):
-        Wcp = np.diag(wcp)
-        N = int(self._T/self._Ts)
-        Nu = int(self._Tu/self._Ts)
-        z = np.zeros((12,N), dtype=np.float64)
-        u = np.zeros((6,N), dtype=np.float64)
-        y = np.zeros((13,N), dtype=np.float64)
+        z = np.zeros((12,self._N), dtype=np.float64)
+        u = np.zeros((6,self._N), dtype=np.float64)
 
         u[:,0] = self._u_step
         z[:,0] = self._z0
 
-        for k in range (0,N-1):
-            z_dot, output = self._continuos_time_model(z[:,k],u[:,k], Wcp)
-            z[:,k+1] = z[:,k] + self._Ts*z_dot
+        for k in range (0,self._N-1):
+            k1 = self._continuos_time_state(z[:,k],u[:,k], Wcv) #z_dot, output = ...
+            z[:,k+1] = z[:,k] + self._Ts*k1
 
-            y[:,k] = output
-            u[:, k+1] = u[:, k] if (k + 1) < Nu else np.zeros((6,), dtype=np.float64)
+            u[:, k+1] = u[:, k] if (k + 1) < self._Nu else np.zeros((6,), dtype=np.float64)
+
+        end = time.perf_counter()
+        print(f"Elapsed time for forward finite difference simulation: {(end - start) * 1e3:.3f} ms")
+
+        return z, u
         
-        _, y[:, -1] = self._continuos_time_model(z[:, -1], u[:, -1], Wcp)
 
-        return z, y, u
-        
+    def rk2_mp_simulation(self, wcv):
+        start = time.perf_counter()
+        Wcv = np.diag(wcv)
 
-    def rk2_simulation(self, wcp):
-        Wcp = np.diag(wcp)
-        N = int(self._T/self._Ts)
-        Nu = int(self._Tu/self._Ts)
-        z = np.zeros((12,N), dtype=np.float64)
-        u = np.zeros((6,N), dtype=np.float64)
-        y = np.zeros((13,N), dtype=np.float64)
+        z = np.zeros((12,self._N), dtype=np.float64)
+        u = np.zeros((6,self._N), dtype=np.float64)
 
         u[:,0] = self._u_step
         z[:,0] = self._z0
 
-        for k in range (0,N-1):
-            z_dot, output = self._continuos_time_model(z[:,k],u[:,k], Wcp)
-            z_mid = z[:,k] + 0.5*self._Ts*z_dot
+        for k in range (0,self._N-1):
+            k1 = self._continuos_time_state(z[:,k],u[:,k], Wcv)
+            k2 = self._continuos_time_state(z[:,k] + 0.5*self._Ts*k1,u[:,k], Wcv)
+            z[:,k+1] = z[:,k] + self._Ts*k2
 
-            z_dot_mid, _ = self._continuos_time_model(z_mid,u[:,k], Wcp)
-            z[:,k+1] = z[:,k] + self._Ts*z_dot_mid
-
-            y[:,k] = output
-            u[:, k+1] = u[:, k] if (k + 1) < Nu else np.zeros((6,), dtype=np.float64)
+            u[:, k+1] = u[:, k] if (k + 1) < self._Nu else np.zeros((6,), dtype=np.float64)
         
-        _, y[:, -1] = self._continuos_time_model(z[:, -1], u[:, -1], Wcp)
+        end = time.perf_counter()
+        print(f"Elapsed time for rk2 middle-point simulation: {(end - start) * 1e3:.3f} ms")
+        
+        return z, u
 
-        return z, y, u
+    def rk4_simulation(self, wcv):
+        start = time.perf_counter()
+        Wcv = np.diag(wcv)
+
+        z = np.zeros((12,self._N), dtype=np.float64)
+        u = np.zeros((6,self._N), dtype=np.float64)
+
+        u[:,0] = self._u_step
+        z[:,0] = self._z0
+
+        for k in range (0,self._N-1):
+            k1 = self._continuos_time_state(z[:,k],u[:,k], Wcv)
+            k2 = self._continuos_time_state(z[:,k] + 0.5*self._Ts*k1,u[:,k], Wcv)
+            k3 = self._continuos_time_state(z[:,k] + 0.5*self._Ts*k2,u[:,k], Wcv)
+            k4 = self._continuos_time_state(z[:,k] +     self._Ts*k3,u[:,k], Wcv)
+            z[:,k+1] = z[:,k] + self._Ts/6*k1 + self._Ts/3*k2 + self._Ts/3*k3 + self._Ts/6*k4
+
+            u[:, k+1] = u[:, k] if (k + 1) < self._Nu else np.zeros((6,), dtype=np.float64)
+        
+        end = time.perf_counter()
+        print(f"Elapsed time for rk4 simulation: {(end - start) * 1e3:.3f} ms")
+        
+        return z, u
+
+    def lagrange_formula(self, wcv): 
+        start = time.perf_counter() 
+
+        z = np.zeros((12, self._N), dtype=np.float64) 
+        u = np.zeros((6, self._N), dtype=np.float64)
+
+        for k in range(self._N):
+            u[:, k] = self._u_step if k < self._Nu else 0.0
+
+        Ts = self._Ts
+        Tu = self._Nu * Ts
+
+        for i in range(6):
+            w = wcv[i]
+            u_step = self._u_step
+
+            # Values at the end of the step
+            v_Tu = u_step * (1.0 - np.exp(-w * Tu))
+            q_Tu = u_step * Tu + (u_step / w) * (np.exp(-w * Tu) - 1.0)
+
+            for k in range(self._N):
+                t = k * Ts
+
+                if k < self._Nu:
+                    # First phase: costant input = u_step
+                    v = u_step * (1.0 - np.exp(-w * t))
+                    q = u_step * t + (u_step / w) * (np.exp(-w * t) - 1.0)
+                else:
+                    # Second phase: null input, yet with non zero initial condition
+                    dt = t - Tu
+                    v = v_Tu * np.exp(-w * dt)
+                    q = q_Tu + (v_Tu / w) * (1.0 - np.exp(-w * dt))
+
+                z[i,   k] = q        
+                z[i+6, k] = v        
+                    
+        end = time.perf_counter() 
+        print(f"Elapsed time for exact simulation: {(end - start) * 1e3:.3f} ms") 
+
+        return z, u
 
 
-q_0 = np.array([0, 0, 0, 0, 0, 0], dtype=np.float64)
-qdot_0 = np.array([0, 0, 0, 0, 0, 0], dtype=np.float64)
+    def compute_output(self, z):
+        y = np.zeros((13,self._N), dtype=np.float64)
 
-z_0 = np.hstack((q_0, qdot_0))
-w_cp = np.array([100,100,100,100,100,100], dtype=np.float64)
+        for k in range (0,self._N):
+            q = z[:6,k]; qdot = z[6:,k]
+            y[:7,k] = self._forward_kinematics(q)
+            y[7:,k] = self._diff_kinematic(q, qdot)
 
-urdf = UrdfLoader('ur5_robot')
-robot = Robot(0.01,5.0,3.0,z_0,1.0,urdf)
-z, y, u = robot.ffd_simulation(w_cp)
+        return y
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
