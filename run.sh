48caab1fdf236ce3330ecba4b03a505b86d3d467
Balin Balinov <bb@Balins-MacBook-Air.local>
2025-11-21 23:28:11 +0100

    cleaned up main a bit

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script to run the MPC with proper library paths
 
-export LD_LIBRARY_PATH=/home/bb/Desktop/projects/robotic-mpc/acados/lib:$LD_LIBRARY_PATH
-export ACADOS_SOURCE_DIR=/home/bb/Desktop/projects/robotic-mpc/acados
+export DYLD_LIBRARY_PATH=/Users/bb/Desktop/robotic-mpc/acados/lib:$LD_LIBRARY_PATH
+export ACADOS_SOURCE_DIR=/Users/bb/Desktop/robotic-mpc/acados
 
 python main.py
 
