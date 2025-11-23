a905445202786250c648bdc51633c75688cb4434
Balin Balinov <bb@Balins-MacBook-Air.local>
2025-11-23 17:01:23 +0100

    changed constraints boundaries

--- c_generated_code/acados_solver_six_dof_robot_model.c
+++ c_generated_code/acados_solver_six_dof_robot_model.c
@@ -488,13 +488,14 @@ void six_dof_robot_model_acados_setup_nlp_in(six_dof_robot_model_solver_capsule*
     // change only the non-zero elements:
     W_0[0+(NY0) * 0] = 100;
     W_0[1+(NY0) * 1] = 100;
-    W_0[2+(NY0) * 2] = 10;
-    W_0[3+(NY0) * 3] = 0.01;
+    W_0[2+(NY0) * 2] = 100;
+    W_0[3+(NY0) * 3] = 100;
     W_0[4+(NY0) * 4] = 0.01;
     W_0[5+(NY0) * 5] = 0.01;
     W_0[6+(NY0) * 6] = 0.01;
     W_0[7+(NY0) * 7] = 0.01;
     W_0[8+(NY0) * 8] = 0.01;
+    W_0[9+(NY0) * 9] = 0.01;
     ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, 0, "W", W_0);
     free(W_0);
     double* yref = calloc(NY, sizeof(double));
@@ -509,13 +510,14 @@ void six_dof_robot_model_acados_setup_nlp_in(six_dof_robot_model_solver_capsule*
     // change only the non-zero elements:
     W[0+(NY) * 0] = 100;
     W[1+(NY) * 1] = 100;
-    W[2+(NY) * 2] = 10;
-    W[3+(NY) * 3] = 0.01;
+    W[2+(NY) * 2] = 100;
+    W[3+(NY) * 3] = 100;
     W[4+(NY) * 4] = 0.01;
     W[5+(NY) * 5] = 0.01;
     W[6+(NY) * 6] = 0.01;
     W[7+(NY) * 7] = 0.01;
     W[8+(NY) * 8] = 0.01;
+    W[9+(NY) * 9] = 0.01;
 
     for (int i = 1; i < N; i++)
     {
@@ -529,10 +531,9 @@ void six_dof_robot_model_acados_setup_nlp_in(six_dof_robot_model_solver_capsule*
 
     double* W_e = calloc(NYN*NYN, sizeof(double));
     // change only the non-zero elements:
-    W_e[0+(NYN) * 0] = 50;
-    W_e[1+(NYN) * 1] = 50;
+    W_e[0+(NYN) * 0] = 100;
+    W_e[1+(NYN) * 1] = 100;
     W_e[2+(NYN) * 2] = 100;
-    W_e[3+(NYN) * 3] = 200;
     ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, N, "W", W_e);
     free(W_e);
     ocp_nlp_cost_model_set_external_param_fun(nlp_config, nlp_dims, nlp_in, 0, "nls_y_fun", &capsule->cost_y_0_fun);
@@ -625,18 +626,18 @@ void six_dof_robot_model_acados_setup_nlp_in(six_dof_robot_model_solver_capsule*
     double* lubu = calloc(2*NBU, sizeof(double));
     double* lbu = lubu;
     double* ubu = lubu + NBU;
-    lbu[0] = -2;
-    ubu[0] = 100;
-    lbu[1] = -2;
-    ubu[1] = 100;
-    lbu[2] = -2;
-    ubu[2] = 100;
-    lbu[3] = -2;
-    ubu[3] = 100;
-    lbu[4] = -2;
-    ubu[4] = 100;
-    lbu[5] = -2;
-    ubu[5] = 100;
+    lbu[0] = -10;
+    ubu[0] = 10;
+    lbu[1] = -10;
+    ubu[1] = 10;
+    lbu[2] = -10;
+    ubu[2] = 10;
+    lbu[3] = -10;
+    ubu[3] = 10;
+    lbu[4] = -10;
+    ubu[4] = 10;
+    lbu[5] = -10;
+    ubu[5] = 10;
 
     for (int i = 0; i < N; i++)
     {
