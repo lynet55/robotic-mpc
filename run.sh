c4f346141f37a61eb98ca8dfc946dcc7e7d2f47e
Balin Balinov <bb@Balins-MacBook-Air.local>
2025-11-23 16:02:49 +0100

    made one forwarskinematics function which exports rotatio matrixes, and one for euler angles

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Script to run the MPC with proper library paths
 
-export DYLD_LIBRARY_PATH=/Users/bb/Desktop/robotic-mpc/acados/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/Users/bb/Desktop/robotic-mpc/acados/lib:${DYLD_LIBRARY_PATH}
+export DYLD_FALLBACK_LIBRARY_PATH=/Users/bb/Desktop/robotic-mpc/acados/lib:${DYLD_FALLBACK_LIBRARY_PATH}
 export ACADOS_SOURCE_DIR=/Users/bb/Desktop/robotic-mpc/acados
 
 python main.py
