8ddae40fad267e1dd92170ce4da391b859dfac60
Balin Balinov <bb@Balins-MacBook-Air.local>
2025-12-11 13:04:28 +0100

    slight clean up main loop

--- simulator.py
+++ simulator.py
@@ -18,13 +18,10 @@ class Simulator:
         self.qdot_0 = qdot_0
         self.q_0 = q_0
         self.wcv = wcv
+        self.initial_state = np.hstack((self.q_0, self.qdot_0))
 
         self.robot_loader = urdf('ur5')
-
-        if scene:
-            self.scene = robot_visualizer(self.robot_loader)
-        else:
-            self.scene = None
+        self.scene = robot_visualizer(self.robot_loader)
 
         self.surface = Surface(
             position=surface_origin,
@@ -33,7 +30,7 @@ class Simulator:
         )
         self.simulation_model = simulation_robot_6dof(
             urdf_loader=self.robot_loader,
-            z0=np.hstack((self.q_0, self.qdot_0)),
+            z0=self.initial_state,
             u0=self.qdot_0,
             dt=self.dt,
             Nsim=self.Nsim,
@@ -47,12 +44,12 @@ class Simulator:
 
         self.mpc = model_predictive_control(
             surface=self.surface,
-            initial_state=np.hstack((self.q_0, self.qdot_0)),
+            initial_state=self.initial_state,
             model=self.prediction_model,
             N_horizon=self.prediction_horizon,
             Tf=self.dt*self.prediction_horizon
         )
-        
+
         self.scene.add_surface_from_casadi(
             self.surface.get_surface_function(),
             x_limits=self.surface.get_limits()[0],
@@ -61,20 +58,20 @@ class Simulator:
             path="surfaces/quadratic_surface",
             color=0x3399FF,
             opacity=0.6,    
-            origin=surface_origin,            
-            orientation_rpy=surface_orientation_rpy,    
+            origin=self.surface_origin,            
+            orientation_rpy=self.surface_orientation_rpy,    
         )
 
+        ee_position_0 = self.simulation_model.ee_position(0)
         self.scene.add_triad(
-            position=self.simulation_model.ee_position(0),
+            position=ee_position_0,
             orientation_rpy=self.simulation_model.ee_orientation(0),
             path="frames/end_effector_frame",
             scale=0.2,
             line_width=2.0
         )
 
-        pose_ee_euler_0 = self.simulation_model.forward_kinematics(self.q_0)
-        self.traj_points = [pose_ee_euler_0[:3].copy()]
+        self.traj_points = [ee_position_0.copy()]
         self.traj_array = np.vstack(self.traj_points)
 
         self.scene.add_line(
@@ -91,29 +88,31 @@ class Simulator:
             current_state = self.simulation_model.state(t)        
             self.mpc.solver.set(0, 'lbx', current_state)
             self.mpc.solver.set(0, 'ubx', current_state)
-            status = self.mpc.solver.solve()
+            self.mpc.solver.solve()
             u = self.mpc.solver.get(0, "u")
 
             #Integration Step
-            self.simulation_model.update(self.simulation_model.state(t), u, t)
-            q1, q2, q3, q4, q5, q6 = self.simulation_model.joint_angles(t+1)
+            self.simulation_model.update(current_state, u, t)
 
             #Visual Update
-            if self.scene is not None:
+            if t % 100 == 0:
+                q1, q2, q3, q4, q5, q6 = self.simulation_model.joint_angles(t+1)
+                ee_pos = self.simulation_model.ee_position(t+1)
+                
                 self.scene.set_joint_angles({
-                "shoulder_pan_joint": q1,
-                "shoulder_lift_joint": q2,
-                "elbow_joint": q3,
-                "wrist_1_joint": q4,
-                "wrist_2_joint": q5,
-                "wrist_3_joint": q6
+                    "shoulder_pan_joint": q1,
+                    "shoulder_lift_joint": q2,
+                    "elbow_joint": q3,
+                    "wrist_1_joint": q4,
+                    "wrist_2_joint": q5,
+                    "wrist_3_joint": q6
                 })
 
-            self.traj_points.append(self.simulation_model.ee_position(t+1).copy())
-            self.traj_array = np.vstack(self.traj_points)
+                self.traj_points.append(ee_pos.copy())
+                self.traj_array = np.vstack(self.traj_points)
 
-            self.scene.update_triad("frames/end_effector_frame", position=self.simulation_model.ee_position(t+1), orientation_rpy=self.simulation_model.ee_orientation(t+1))
-            self.scene.update_line(path="lines/ee_trajectory",points=self.traj_array)
+                self.scene.update_triad("frames/end_effector_frame", position=ee_pos, orientation_rpy=self.simulation_model.ee_orientation(t+1))
+                self.scene.update_line(path="lines/ee_trajectory", points=self.traj_array)
 
     def get_results(self):
         data = {
@@ -127,9 +126,9 @@ class Simulator:
 
 if __name__ == "__main__":
     sim0 = Simulator(
-        dt=0.01,
+        dt=0.001,
         prediction_horizon=200,
-        simulation_time=1,
+        simulation_time=4000,
         surface_limits=((-2, 2), (-2, 2)),
         surface_origin=np.array([0.0, 0.0, 0.0]),
         surface_orientation_rpy=np.array([0.0, 0.0, 0.0]),
@@ -138,6 +137,16 @@ if __name__ == "__main__":
         wcv=np.array([5,10,15,20,25,35]),
         scene=True
     )
+
+    #paper weights
+    # sim0.mpc.w_origin_task = 200.0
+    # sim0.mpc.w_normal_alignment_task = 50.0
+    # sim0.mpc.w_x_alignment_task = 300.0
+    # sim0.mpc.w_fixed_x_task = 200.0
+    # sim0.mpc.w_fixed_vy_task = 100.0
+    # sim0.mpc.w_u = 0.01
+
+
     sim0.run()
     results = sim0.get_results()
     print(results)
