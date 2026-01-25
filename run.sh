e6172dd34a143e105787a2f52c093ade7253b0d5
Daviderisolo <risolodaviderisolo@gmail.com>
2026-01-25 12:37:16 +0100

    Fix model mismatch in simulator and report generation

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Path alle librerie acados
-export LD_LIBRARY_PATH=/home/daviderisolo/robotic-mpc/acados/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/root/robotic-mpc/acados/lib:$LD_LIBRARY_PATH
 
 # Path alla cartella sorgente acados (serve ad acados_ocp_solver)
-export ACADOS_SOURCE_DIR=/home/daviderisolo/robotic-mpc/acados
+export ACADOS_SOURCE_DIR=/root/robotic-mpc/acados
 
 # export DYLD_LIBRARY_PATH=/Users/bb/Desktop/robotic-mpc/acados/lib:${DYLD_LIBRARY_PATH}
 # export DYLD_FALLBACK_LIBRARY_PATH=/Users/bb/Desktop/robotic-mpc/acados/lib:${DYLD_FALLBACK_LIBRARY_PATH}
